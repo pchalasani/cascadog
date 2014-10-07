aa48b682d1da983199106e92d5a2969a59c2a3e8
Prasad Chalasani <prasadch@yahoo-inc.com>
2014-10-07 09:16:20 -0400

    project.clj - various tweaks to get serialization to work

--- project.clj
+++ project.clj
@@ -9,6 +9,9 @@
   :jvm-opts ["-Xmx768m"
              "-server"
              "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
+  :javac-options ["-target" "1.6" "-source" "1.6"]
+  :aot [cascadog.core]
+  :main cascadog.core
   :source-paths ["src"]
   :repositories {"conjars" "http://conjars.org/repo"}
   :exclusions [log4j/log4j org.slf4j/slf4j-log4j12]
@@ -17,9 +20,23 @@
                  [org.clojure/tools.macro "0.1.2"]
                  [log4j "1.2.16"]
                  [org.slf4j/slf4j-log4j12 "1.6.6"]
-                 ]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+                 [cascading/cascading-hadoop "2.5.3"
+                  :exclusions [org.codehaus.janino/janino
+                               org.apache.hadoop/hadoop-core]]
+                 [com.twitter/chill-hadoop "0.3.5"]
+                 [com.twitter/carbonite "1.4.0"]
+                 [com.taoensso/nippy "2.6.3"]  ;; serialization
+                 [com.twitter/maple "0.2.2"]
+                 [jackknife "0.1.7"]
+                 [hadoop-util "0.3.0"]]
+  :profiles {
+             ;; :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             ;; :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             ;; :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :provided {:dependencies [[org.apache.hadoop/hadoop-core "1.2.1"]]}
              :dev {:resource-paths ["dev"]
                    :plugins [[lein-midje "3.1.3"]]
-                   :dependencies  [[cascalog/midje-cascalog "2.1.1"]]}})
+                   :dependencies
+                   [[cascalog/midje-cascalog "2.1.1"]
+                    [org.apache.hadoop/hadoop-core "1.2.1"]
+                    ]}})
